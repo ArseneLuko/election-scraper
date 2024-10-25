07d942069c192ea1857157ec48c586216a0159e9
ArseneLuko <arsene.luko@gmail.com>
2024-10-25 21:09:35 +0200

    Initializing

---
+++ scraper.py
@@ -0,0 +1,15 @@
+"""
+scraper.py: třetí projekt do Engeto Online Python Akademie
+author: Lukáš Karásek
+email: lukas@lukaskarasek.cz
+discord: lukaskarasek__77224
+"""
+
+import requests
+
+link_elections = "https://www.volby.cz/pls/ps2017nss/ps3?xjazyk=CZ"
+
+# 
+
+if __name__ == "__main__":
+    pass
